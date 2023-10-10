1be724f98fda9718d7d16a9cc06964dfb2ddc4d5
Anne Jan Brouwer <brouwer@annejan.com>
2023-10-10 15:46:22 +0200

    Optimised

--- find_document_number.py
+++ find_document_number.py
@@ -2,59 +2,67 @@ import argparse
 import fitz  # PyMuPDF
 import re
 import subprocess
+import os
 
-tess = "tesseract stdin stdout --psm 7 -l nld"
+TESS_COMMAND = "tesseract stdin stdout --psm 7 -l nld"
 
-# Function to extract structured data from a PDF file
+
+def extract_text_from_image(image_bytes):
+    rc = subprocess.run(
+        TESS_COMMAND,
+        input=image_bytes,
+        stdout=subprocess.PIPE,
+        shell=True,
+        stderr=subprocess.DEVNULL,
+    )
+    return rc.stdout.decode()
+
+
+def extract_text_from_pdf(pdf_document, page_number, rect):
+    page = pdf_document[page_number]
+    text = page.get_textbox(rect)
+    return text
+
+
+def process_page(pdf_document, page_number, viewbox):
+    width, height = pdf_document[0].mediabox_size
+    left, top, right, bottom = viewbox
+
+    if left < 0:
+        left = width + left
+
+    if right < 0:
+        right = width + right
+
+    if top < 0:
+        top = height + top
+
+    if bottom < 0:
+        bottom = height + bottom
+
+    rect = fitz.Rect(left, top, right, bottom)
+    return rect
+
+
 def extract_pdf_data(pdf_file_path, viewbox):
-    # Open the PDF file
-    pdf_document = fitz.open(pdf_file_path)
-    # Iterate through each page in the PDF
-    for page_number in range(pdf_document.page_count):
-        page = pdf_document[page_number]
-        width = page.mediabox_size.x
-        height = page.mediabox_size.y
-        left, top, right, bottom = viewbox
-
-        if left < 0:
-            left = width + left
-
-        if right < 0:
-            right = width + right
-
-        if top < 0:
-            top = height + top
-
-        if bottom < 0:
-            bottom = height + bottom
-
-        rect = fitz.Rect(left, top, right, bottom)
-        #        print(rect)
-        text = page.get_textbox(rect)
-        #        pix = page.get_pixmap(clip=rect)
-        #        pix.save(f"{page_number}.png")
-        match = re.search(r"\d+[A-Za-z]*", text)
-        if match:
-            print(f"Document: {match.group()} on page: {page_number+1}")
-        else:
-            pix = page.get_pixmap(clip=rect)
-            rc = subprocess.run(
-                tess,
-                input=pix.tobytes("png"),
-                stdout=subprocess.PIPE,
-                shell=True,
-                stderr=subprocess.DEVNULL,
-            )
-            text = rc.stdout.decode()
-            match = re.search(r"\d+[A-Za-z]*", text)
-            if match:
-                print(f"Document: {match.group()} on page: {page_number+1}")
-
-    # Close the PDF document
-    pdf_document.close()
+    try:
+        with fitz.open(pdf_file_path) as pdf_document:
+            for page_number in range(pdf_document.page_count):
+                rect = process_page(pdf_document, page_number, viewbox)
+                text = extract_text_from_pdf(pdf_document, page_number, rect)
+                match = re.search(r"\d+[A-Za-z]*", text)
+                if match:
+                    print(f"Document: {match.group()} on page: {page_number + 1}")
+                else:
+                    pix = pdf_document[page_number].get_pixmap(clip=rect)
+                    text = extract_text_from_image(pix.tobytes("png"))
+                    match = re.search(r"\d+[A-Za-z]*", text)
+                    if match:
+                        print(f"Document: {match.group()} on page: {page_number + 1}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
 
-# Define command-line arguments
 def main():
     parser = argparse.ArgumentParser(
         description="Extract structured data from a PDF file."
@@ -70,6 +78,10 @@ def main():
 
     args = parser.parse_args()
     pdf_file_path = args.pdf_file
+    if not os.path.isfile(pdf_file_path):
+        print(f"Error: The specified PDF file '{pdf_file_path}' does not exist.")
+        return
+
     viewbox = args.viewbox if args.viewbox else [-180, 20, -20, 120]  # Default viewbox
 
     extract_pdf_data(pdf_file_path, viewbox)
