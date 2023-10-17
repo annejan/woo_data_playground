dad58577c27479eee5b2cb34575ac5d3a4ed5cd8
Anne Jan Brouwer <brouwer@annejan.com>
2023-10-17 21:13:56 +0200

    refactor

--- get_pdfs.py
+++ get_pdfs.py
@@ -3,6 +3,19 @@ import requests
 from bs4 import BeautifulSoup
 
 
+def get_json_data(json_url):
+    try:
+        response = requests.get(json_url)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            print(f"Failed to retrieve JSON data. Status code: {response.status_code}")
+            return None
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
+        return None
+
+
 def get_pdf_links_from_webpage(url):
     pdfs = []
     try:
@@ -12,12 +25,12 @@ def get_pdf_links_from_webpage(url):
             soup = BeautifulSoup(response.text, "html.parser")
             # Find all the anchor tags (links) in the HTML
             links = soup.find_all("a")
-            # Extract and print the href attribute of each link
-            for link in links:
-                href = link.get("href")
-                if href:
-                    if href.endswith(".pdf"):
-                        pdfs.append(href)
+            # Extract and return the href attribute of each link that ends with ".pdf"
+            pdfs = [
+                link.get("href")
+                for link in links
+                if link.get("href", "").endswith(".pdf")
+            ]
         else:
             print(
                 f"Failed to retrieve the webpage. Status code: {response.status_code}"
@@ -27,24 +40,31 @@ def get_pdf_links_from_webpage(url):
     return pdfs
 
 
+def download_pdf(url, save_path):
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            with open(save_path, "wb") as file:
+                file.write(response.content)
+            print(f"File downloaded and saved as {save_path}")
+        else:
+            print(f"Failed to download the file. Status code: {response.status_code}")
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
 
-if __name__ == "__main__":
+
+def main():
     base_url = "https://wobcovid19.rijksoverheid.nl/publicaties/"
     json_url = "https://do-ams3-17.hw.webhare.net/services/wobcovid19-prod-v2-1/search/?first=0&count=300&orderby=publicationdate"
+    data = get_json_data(json_url)
 
-    response = requests.get(json_url)
-    if response.status_code == 200:
-        data = response.json()
-        # Check if the number of results matches totalcount
+    if data:
         if len(data.get("results", [])) == data.get("totalcount", 0):
-            # Initialize a dictionary to store link and title as key-value pairs
             link_title_pairs = {}
-            # Loop through the results
             for result in data.get("results", []):
-                pub = result.get("link").lstrip('#')
+                pub = result.get("link").lstrip("#")
                 title = result.get("publication", {}).get("title")
                 if pub and title:
-                    # Add the link and title as key-value pairs to the dictionary
                     link_title_pairs[pub] = title
 
             for pub, title in link_title_pairs.items():
@@ -53,19 +73,15 @@ if __name__ == "__main__":
                     os.mkdir(pub)
                 with open(f"{pub}/title.txt", "w") as file:
                     file.write(title)
-                pdfs = get_pdf_links_from_webpage(f"{base_url}{pub}/")
-                for pdf in pdfs:
-                    try:
-                        response = requests.get(f"{base_url}{pub}/{pdf}")
-                        if response.status_code == 200:
-                            with open(f"{pub}/{pdf}", "wb") as file:
-                                file.write(response.content)
-                            print(f"File downloaded and saved as {pub}/{pdf}")
-                        else:
-                            print(f"Failed to download the file. Status code: {response.status_code}")
-                    except requests.exceptions.RequestException as e:
-                        print(f"An error occurred: {e}")
+                pdf_links = get_pdf_links_from_webpage(f"{base_url}{pub}/")
+                for pdf in pdf_links:
+                    pdf_url = f"{base_url}{pub}/{pdf}"
+                    download_pdf(pdf_url, f"{pub}/{pdf}")
         else:
-            print("The result count does not match the totalcount, please fix json_url.")
-    else:
-        print(f"Failed to retrieve JSON data. Status code: {response.status_code}")
+            print(
+                "The result count does not match the totalcount, please fix json_url."
+            )
+
+
+if __name__ == "__main__":
+    main()
