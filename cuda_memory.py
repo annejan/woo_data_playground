ffa061b0cca9558eaa3452fdf9b939878bd6e026
Anne Jan Brouwer <brouwer@annejan.com>
2023-10-22 00:32:08 +0200

    Graph memory

--- cuda_memory.py
+++ cuda_memory.py
@@ -1,45 +1,44 @@
 import time
-from tqdm import tqdm
-import asciichartpy
+from collections import deque
 import pynvml
+import shutil
+import asciichartpy
+import os
 
-# Initialize NVIDIA GPU monitoring
 pynvml.nvmlInit()
-handle = pynvml.nvmlDeviceGetHandleByIndex(0)  # Assuming you are monitoring GPU 0
+handle = pynvml.nvmlDeviceGetHandleByIndex(0)
 
 def get_gpu_memory():
-    """Retrieve the current GPU memory usage in MB"""
+    """Retrieve the current GPU memory usage in GB."""
     meminfo = pynvml.nvmlDeviceGetMemoryInfo(handle)
-    return meminfo.used / (1024**2)
+    return meminfo.used / (1024**3)  # Convert to GB
 
-def monitor_memory_usage(duration, interval):
-    """
-    Monitor memory usage over time.
-    
-    :param duration: Total time duration for monitoring (seconds)
-    :param interval: Time interval for each data collection (seconds)
-    :return: List of memory usage data points
-    """
-    data_points = []
-    num_iterations = int(duration / interval)
-    
-    for _ in tqdm(range(num_iterations), desc="Monitoring GPU memory", ncols=100):
-        mem_used = get_gpu_memory()
-        data_points.append(mem_used)
-        time.sleep(interval)
+def console_size():
+    """Retrieve the width and height of the console."""
+    width, height = shutil.get_terminal_size((20, 50))
+    return width, height
 
-    return data_points
+def monitor_memory(interval=1):
+    """Monitor and plot GPU memory usage in real-time."""
+    width, height = console_size()
+    # Number of points to keep is based on console width
+    data_points = deque(maxlen=width - 20)
+
+    try:
+        while True:
+            mem_used = get_gpu_memory()
+            data_points.append(mem_used)
+
+            _, height = console_size()  # Update height in case terminal is resized
+            os.system('cls' if os.name == 'nt' else 'clear')
+            print(asciichartpy.plot(list(data_points), {'height': height - 5}))  # Leaving some margin for plot labels
+
+            time.sleep(interval)
+    except KeyboardInterrupt:
+        print("\nMonitoring stopped.")
 
 if __name__ == "__main__":
-    # Collect memory usage data over 10 seconds with 1-second intervals
-    data = monitor_memory_usage(10, 1)
-    
-    # Clear the screen
-    print("\033[H\033[J")
-    
-    # Plot memory usage
-    print("Memory usage over time (in MB):\n")
-    print(asciichartpy.plot(data, {"height": 15}))
+    monitor_memory()
 
-# Shutdown NVIDIA GPU monitoring
 pynvml.nvmlShutdown()
+
