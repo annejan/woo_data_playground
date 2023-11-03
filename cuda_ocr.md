fc4a8d0a1fb5f0109805f82ef43348b4e7000cec
Anne Jan Brouwer <brouwer@annejan.com>
2023-11-03 23:04:46 +0100

    Updated CUDA OCR

---
+++ cuda_ocr.md
@@ -0,0 +1,43 @@
+# CUDA OCR PDF Processing
+
+This Python script utilizes `easyocr` to perform Optical Character Recognition (OCR) on PDF files and convert them into text. It's designed to handle multiple PDF files using a glob pattern and supports configurable DPI and language settings.
+
+## Requirements
+
+- Python 3.6 or higher
+- easyocr
+- pdf2image
+- numpy
+
+Install the required packages using pip:
+
+```bash
+pip install easyocr pdf2image numpy
+```
+
+## Usage
+
+Run the script from the command line, providing the glob pattern for the PDF files you want to convert, and optional arguments for DPI and language:
+
+```bash
+python pdf_ocr_script.py 'path/to/pdf/files/*.pdf' --dpi 600 --lang 'nl'
+```
+
+### Arguments
+
+- `pdf_glob`: The glob pattern for the PDF files to convert (e.g., `'./pdfs/*.pdf'`).
+- `--dpi` (optional): The resolution used for converting PDF pages to images before OCR. Default is `600`.
+- `--lang` (optional): The language code used by OCR. Default is Dutch (`'nl'`), but you can set it to any supported language like English (`'en'`).
+
+### Output
+
+The script will create a text file for each PDF file processed, containing the extracted text. These text files will be saved in the same directory as the PDF files, with the original filename suffixed with `_ocr.txt`.
+
+## License
+
+This package is open-source and released under the [European Union Public License version 1.2](https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12).
+You are free to use, modify, and distribute the package in accordance with the terms of the license.
+
+## Contributing
+
+For contributions, bug reports, or suggestions, please visit the project repository on GitHub.
--- cuda_ocr.py
+++ cuda_ocr.py
@@ -1,55 +1,62 @@
 import easyocr
 import sys
 import argparse
+import glob
 from pdf2image import convert_from_path
 import numpy as np
 
 
 def create_arg_parser():
     """Create and return the ArgumentParser object for command line options."""
-    parser = argparse.ArgumentParser(description="Convert a PDF to text using OCR.")
-    parser.add_argument("pdf_file", help="The PDF file to be converted.")
+    parser = argparse.ArgumentParser(description="Convert PDF files to text using OCR.")
+    parser.add_argument(
+        "pdf_glob", help="The glob pattern for PDF files to be converted."
+    )
     parser.add_argument(
         "--dpi", type=int, default=600, help="DPI for image conversion, default is 600."
     )
     parser.add_argument(
-        "--lang", default="nl", help="Language code(s) for OCR, default is 'nl'."
+        "--lang", default="nl", help="Language code for OCR, default is 'nl'."
     )
     return parser
 
 
-def perform_ocr_on_pdf(pdf_path, dpi=600, language="nl"):
-    """Perform OCR on the given PDF and return the extracted text."""
-    reader = easyocr.Reader([language], gpu=True)
-    pages = convert_from_path(pdf_path, dpi=dpi, fmt="jpeg", thread_count=4)
+def perform_ocr_on_page(page_image, reader):
+    """Perform OCR on a single page image."""
+    ocr_results = reader.readtext(np.array(page_image))
+    page_text = " ".join(result[1] for result in ocr_results)
+    return page_text
 
+
+def process_pdf(pdf_path, reader, dpi):
+    """Process a single PDF file, performing OCR on each page."""
+    pages = convert_from_path(pdf_path, dpi=dpi, fmt="jpeg", thread_count=4)
     full_text = []
 
     for page_number, page_image in enumerate(pages, start=1):
         print(f"Processing page {page_number}...")
-        ocr_results = reader.readtext(np.array(page_image))
-        page_text = " ".join(result[1] for result in ocr_results)
+        page_text = perform_ocr_on_page(page_image, reader)
         full_text.append(page_text)
 
     return "\n\n".join(full_text)
 
 
 def main():
+    """Process PDFs and handle config"""
     arg_parser = create_arg_parser()
     args = arg_parser.parse_args()
 
-    pdf_file_path = args.pdf_file
-    dpi_setting = args.dpi
-    ocr_language = args.lang
+    # Initialize the OCR reader instance outside of the loop
+    reader = easyocr.Reader([args.lang], gpu=True)
 
-    extracted_text = perform_ocr_on_pdf(
-        pdf_file_path, dpi=dpi_setting, language=ocr_language
-    )
+    for pdf_file_path in glob.glob(args.pdf_glob):
+        print(f"Starting OCR for {pdf_file_path}")
+        extracted_text = process_pdf(pdf_file_path, reader, args.dpi)
 
-    text_file_path = pdf_file_path.replace(".pdf", "_ocr.txt")
-    with open(text_file_path, "w", encoding="utf-8") as text_file:
-        text_file.write(extracted_text)
-        print(f"Extracted text written to {text_file_path}")
+        text_file_path = pdf_file_path.replace(".pdf", "_ocr.txt")
+        with open(text_file_path, "w", encoding="utf-8") as text_file:
+            text_file.write(extracted_text)
+            print(f"Extracted text written to {text_file_path}")
 
 
 if __name__ == "__main__":
