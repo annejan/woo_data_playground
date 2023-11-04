48b9143b824e398bcd63aeed0b547be604cf6f02
Anne Jan Brouwer <brouwer@annejan.com>
2023-11-04 01:41:10 +0100

    Paragraph wise and batch_size 8

--- cuda_ocr.py
+++ cuda_ocr.py
@@ -1,3 +1,41 @@
+"""
+PDF to Text OCR Converter
+
+This module provides a command-line interface for converting PDF files to text using OCR (Optical Character Recognition).
+It leverages the EasyOCR library for OCR tasks and PyMuPDF for efficient extraction of images from PDF pages. The script
+processes each page of the given PDF file(s), performs OCR, and saves the extracted text into corresponding text files.
+
+Requirements:
+- easyocr: For OCR operations.
+- PyMuPDF (fitz): For handling PDF and image data.
+
+Usage:
+    python cuda_ocr.py file1.pdf file2.pdf --dpi 300 --lang en
+
+Arguments:
+- pdf_files: One or more PDF files to be processed.
+- --dpi: The dots per inch setting for the image extraction process. Default is 600.
+- --lang: The language code to be used by OCR. Default is 'nl' (Dutch).
+
+Output:
+The script will output a text file with the same name as the input PDF file, appended with "_ocr.txt". For example,
+if the input file is "document.pdf", the output will be "document_ocr.txt".
+
+Example:
+    To convert a single PDF file to text with OCR in English and a DPI of 300:
+        python cuda_ocr.py document.pdf --dpi 300 --lang en
+
+    To convert multiple PDF files in folder "docs" to text with OCR in Dutch and the default DPI:
+        python cuda_ocr.py docs/*.py 
+
+
+This package is open-source and released under the European Union Public License version 1.2.
+You are free to use, modify, and distribute the package in accordance with the terms of the license.
+
+For contributions, bug reports, or suggestions, please visit the project repository on GitHub.
+
+SPDX-License-Identifier: EUPL-1.2
+"""
 import easyocr
 import sys
 import argparse
@@ -19,8 +57,8 @@ def create_arg_parser():
 
 def perform_ocr_on_page(page_image, reader):
     """Perform OCR on a single page image."""
-    ocr_results = reader.readtext(page_image, detail = 0)
-    page_text = " ".join(ocr_results)
+    ocr_results = reader.readtext(page_image, detail = 0, paragraph=True, batch_size = 8)
+    page_text = "\n".join(ocr_results)
     return page_text
 
 
@@ -28,17 +66,19 @@ def process_pdf(pdf_path, reader, dpi):
     """Process a single PDF file, performing OCR on each page."""
     doc = fitz.open(pdf_path)
     full_text = []
-    n = 0
-    print(f"Pages {len(doc)}")
-    for page in doc:
-        n = n + 1
-        print(f"Processing page {n:04}")
-        pix = page.get_pixmap(matrix=fitz.Matrix(dpi / 72, dpi / 72))
-        page_text = perform_ocr_on_page(pix.tobytes("png"), reader)
-        full_text.append(page_text)
-
+    for page_num, page in enumerate(doc, start=1):
+        print(f"Processing page {page_num:04}")
+        scale = dpi / 72
+        matrix = fitz.Matrix(scale, scale)
+        pix = page.get_pixmap(matrix=matrix)
+        try:
+            page_text = perform_ocr_on_page(pix.tobytes("png"), reader)
+            full_text.append(page_text)
+        except Exception as e:
+            print(f"An error occurred while processing page {page_num:04}: {e}")
+        finally:
+            pix = None  # Free pixmap memory immediately
     return "\n\n".join(full_text)
-
 
 def main():
     """Process PDFs and handle config"""
